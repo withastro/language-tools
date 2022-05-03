d0ad1e789b7b4e61fa1f4b226ce4b7ff6e088540
Princesseuh <princssdev@gmail.com>
2022-05-03 17:32:26 -0400

    Implement code actions inside script tags

--- packages/language-server/src/plugins/typescript/features/CodeActionsProvider.ts
+++ packages/language-server/src/plugins/typescript/features/CodeActionsProvider.ts
@@ -1,5 +1,5 @@
 import { flatten } from 'lodash';
-import ts, { CodeFixAction } from 'typescript';
+import ts, { CodeFixAction, FileTextChanges } from 'typescript';
 import { CancellationToken } from 'vscode-languageserver';
 import {
 	CodeAction,
@@ -16,8 +16,14 @@ import { AstroDocument, getLineAtPosition, mapRangeToOriginal } from '../../../c
 import { modifyLines } from '../../../utils';
 import { CodeActionsProvider } from '../../interfaces';
 import { LanguageServiceManager } from '../LanguageServiceManager';
-import { AstroSnapshotFragment } from '../snapshots/DocumentSnapshot';
-import { checkEndOfFileCodeInsert, convertRange, removeAstroComponentSuffix, toVirtualAstroFilePath } from '../utils';
+import { AstroSnapshot, AstroSnapshotFragment, ScriptTagDocumentSnapshot } from '../snapshots/DocumentSnapshot';
+import {
+	checkEndOfFileCodeInsert,
+	convertRange,
+	getScriptTagSnapshot,
+	removeAstroComponentSuffix,
+	toVirtualAstroFilePath,
+} from '../utils';
 import { codeActionChangeToTextEdit } from './CompletionsProvider';
 import { findContainingNode } from './utils';
 
@@ -38,9 +44,6 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 		const filePath = toVirtualAstroFilePath(tsDoc.filePath);
 		const fragment = await tsDoc.createFragment();
 
-		const start = fragment.offsetAt(fragment.getGeneratedPosition(range.start));
-		const end = fragment.offsetAt(fragment.getGeneratedPosition(range.end));
-
 		const tsPreferences = await this.configManager.getTSPreferences(document);
 		const formatOptions = await this.configManager.getTSFormatConfig(document);
 
@@ -51,19 +54,19 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 		}
 
 		if (context.only?.[0] === CodeActionKind.SourceOrganizeImports) {
-			return await this.organizeSortImports(document, false, cancellationToken);
+			return await this.organizeSortImports(document, false, tsPreferences, formatOptions, cancellationToken);
 		}
 
 		// The difference between Sort Imports and Organize Imports is that Sort Imports won't do anything destructive.
 		// For example, it won't remove unused imports whereas Organize Imports will
 		if (context.only?.[0] === sortImportKind) {
-			return await this.organizeSortImports(document, true, cancellationToken);
+			return await this.organizeSortImports(document, true, tsPreferences, formatOptions, cancellationToken);
 		}
 
 		if (context.only?.[0] === CodeActionKind.Source) {
 			return [
-				...(await this.organizeSortImports(document, true, cancellationToken)),
-				...(await this.organizeSortImports(document, false, cancellationToken)),
+				...(await this.organizeSortImports(document, true, tsPreferences, formatOptions, cancellationToken)),
+				...(await this.organizeSortImports(document, false, tsPreferences, formatOptions, cancellationToken)),
 			];
 		}
 
@@ -73,14 +76,48 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 				// We currently cannot support quick fix for unreachable code properly due to the way our TSX output is structured
 				.filter((code) => code !== 7027);
 
-			let codeFixes = errorCodes.includes(2304)
-				? this.getComponentQuickFix(start, end, lang, filePath, formatOptions, tsPreferences)
-				: undefined;
-			codeFixes =
-				codeFixes ?? lang.getCodeFixesAtPosition(filePath, start, end, errorCodes, formatOptions, tsPreferences);
+			const html = document.html;
+			const node = html.findNodeAt(document.offsetAt(range.start));
+
+			let codeFixes: readonly ts.CodeFixAction[] | undefined;
+			let isInsideScript = false;
+
+			if (node.tag === 'script') {
+				const { snapshot: scriptTagSnapshot, filePath: scriptFilePath } = getScriptTagSnapshot(
+					tsDoc as AstroSnapshot,
+					document,
+					node
+				);
+
+				const start = scriptTagSnapshot.offsetAt(scriptTagSnapshot.getGeneratedPosition(range.start));
+				const end = scriptTagSnapshot.offsetAt(scriptTagSnapshot.getGeneratedPosition(range.end));
+
+				codeFixes = lang.getCodeFixesAtPosition(scriptFilePath, start, end, errorCodes, formatOptions, tsPreferences);
+
+				codeFixes = codeFixes.map((fix) => ({
+					...fix,
+					changes: mapScriptTagFixToOriginal(fix.changes, scriptTagSnapshot),
+				}));
+
+				isInsideScript = true;
+			} else {
+				const start = fragment.offsetAt(fragment.getGeneratedPosition(range.start));
+				const end = fragment.offsetAt(fragment.getGeneratedPosition(range.end));
+
+				codeFixes = errorCodes.includes(2304)
+					? this.getComponentQuickFix(start, end, lang, filePath, formatOptions, tsPreferences)
+					: undefined;
+				codeFixes =
+					codeFixes ?? lang.getCodeFixesAtPosition(filePath, start, end, errorCodes, formatOptions, tsPreferences);
+			}
 
 			const codeActions = codeFixes.map((fix) =>
-				codeFixToCodeAction(fix, context.diagnostics, context.only ? CodeActionKind.QuickFix : CodeActionKind.Empty)
+				codeFixToCodeAction(
+					fix,
+					context.diagnostics,
+					context.only ? CodeActionKind.QuickFix : CodeActionKind.Empty,
+					isInsideScript
+				)
 			);
 
 			result.push(...codeActions);
@@ -88,19 +125,38 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 
 		return result;
 
-		function codeFixToCodeAction(codeFix: CodeFixAction, diagnostics: Diagnostic[], kind: CodeActionKind): CodeAction {
+		function codeFixToCodeAction(
+			codeFix: CodeFixAction,
+			diagnostics: Diagnostic[],
+			kind: CodeActionKind,
+			isInsideScript: boolean
+		): CodeAction {
 			const documentChanges = codeFix.changes.map((change) => {
 				return TextDocumentEdit.create(
 					OptionalVersionedTextDocumentIdentifier.create(document.getURL(), null),
 					change.textChanges.map((edit) => {
 						let originalRange = mapRangeToOriginal(fragment, convertRange(fragment, edit.span));
 
-						if (codeFix.fixName === 'import') {
-							return codeActionChangeToTextEdit(document, fragment as AstroSnapshotFragment, edit);
-						}
+						// Inside scripts, we don't need to restrain the insertion of code inside a specific zone as it will be
+						// restricted to the area of the script tag by default
+						if (!isInsideScript) {
+							if (codeFix.fixName === 'import') {
+								return codeActionChangeToTextEdit(document, fragment as AstroSnapshotFragment, edit);
+							}
 
-						if (codeFix.fixName === 'fixMissingFunctionDeclaration') {
-							originalRange = checkEndOfFileCodeInsert(originalRange, document);
+							if (codeFix.fixName === 'fixMissingFunctionDeclaration') {
+								originalRange = checkEndOfFileCodeInsert(originalRange, document);
+							}
+						} else {
+							// Make sure new imports are not added on the file line of the script tag
+							if (codeFix.fixName === 'import') {
+								const existingLine = getLineAtPosition(document.positionAt(edit.span.start), document.getText());
+								const isNewImport = !existingLine.trim().startsWith('import');
+
+								if (!(edit.newText.startsWith('\n') || edit.newText.startsWith('\r\n')) && isNewImport) {
+									edit.newText = ts.sys.newLine + edit.newText;
+								}
+							}
 						}
 
 						return TextEdit.replace(originalRange, edit.newText);
@@ -119,6 +175,20 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 			codeAction.diagnostics = diagnostics;
 
 			return codeAction;
+		}
+
+		function mapScriptTagFixToOriginal(changes: FileTextChanges[], scriptTagSnapshot: ScriptTagDocumentSnapshot) {
+			return changes.map((change) => {
+				change.textChanges.map((edit) => {
+					edit.span.start = fragment.offsetAt(
+						scriptTagSnapshot.getOriginalPosition(scriptTagSnapshot.positionAt(edit.span.start))
+					);
+
+					return edit;
+				});
+
+				return change;
+			});
 		}
 	}
 
@@ -175,6 +245,8 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 	private async organizeSortImports(
 		document: AstroDocument,
 		skipDestructiveCodeActions = false,
+		tsPreferences: ts.UserPreferences,
+		formatOptions: ts.FormatCodeSettings,
 		cancellationToken: CancellationToken | undefined
 	): Promise<CodeAction[]> {
 		if (document.astroMeta.frontmatter.state !== 'closed') {
@@ -190,7 +262,47 @@ export class CodeActionsProviderImpl implements CodeActionsProvider {
 			return [];
 		}
 
-		const changes = lang.organizeImports({ fileName: filePath, type: 'file', skipDestructiveCodeActions }, {}, {});
+		let changes: ts.FileTextChanges[] = [];
+
+		changes.push(
+			...lang.organizeImports(
+				{ fileName: filePath, type: 'file', skipDestructiveCodeActions },
+				formatOptions,
+				tsPreferences
+			)
+		);
+
+		document.scriptTags.forEach((scriptTag) => {
+			const { filePath: scriptFilePath, snapshot: scriptTagSnapshot } = getScriptTagSnapshot(
+				tsDoc as AstroSnapshot,
+				document,
+				scriptTag.container
+			);
+
+			const edits = lang.organizeImports(
+				{ fileName: scriptFilePath, type: 'file', skipDestructiveCodeActions },
+				formatOptions,
+				tsPreferences
+			);
+
+			edits.forEach((edit) => {
+				edit.textChanges = edit.textChanges
+					// Since our last line is a (virtual) export, organize imports will try to rewrite it, so let's only take
+					// changes that actually happens inside the script tag
+					.filter((change) => scriptTagSnapshot.isInGenerated(scriptTagSnapshot.positionAt(change.span.start)))
+					.map((change) => {
+						change.span.start = fragment.offsetAt(
+							scriptTagSnapshot.getOriginalPosition(scriptTagSnapshot.positionAt(change.span.start))
+						);
+
+						return change;
+					});
+
+				return edit;
+			});
+
+			changes.push(...edits);
+		});
 
 		const documentChanges = changes.map((change) => {
 			return TextDocumentEdit.create(
