0715d6c9171417900196be2ec48498a285b4384f
Princesseuh <princssdev@gmail.com>
2022-05-25 10:29:31 -0400

    Readd ignoring diagnostic about module having implicitely an any type

--- packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
+++ packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
@@ -25,6 +25,7 @@ enum DiagnosticCodes {
 	ISOLATED_MODULE_COMPILE_ERR = 1208, // '{0}' cannot be compiled under '--isolatedModules' because it is considered a global script file.
 	TYPE_NOT_ASSIGNABLE = 2322, // Type '{0}' is not assignable to type '{1}'.
 	JSX_NO_CLOSING_TAG = 17008, // JSX element '{0}' has no corresponding closing tag.
+	NO_DECL_IMPLICIT_ANY_TYPE = 7016, // Could not find a declaration file for module '{0}'. '{1}' implicitly has an 'any' type.
 }
 
 export class DiagnosticsProviderImpl implements DiagnosticsProvider {
@@ -104,6 +105,7 @@ export class DiagnosticsProviderImpl implements DiagnosticsProvider {
 					isNoSpreadExpected(diag, document) &&
 					isNoCantReturnOutsideFunction(diag) &&
 					isNoIsolatedModuleError(diag) &&
+					isNoImportImplicitAnyType(diag) &&
 					isNoJsxCannotHaveMultipleAttrsError(diag)
 				);
 			})
@@ -206,6 +208,10 @@ function isNoJsxCannotHaveMultipleAttrsError(diagnostic: Diagnostic) {
 /** Astro allows component with multiple root elements */
 function isNoJSXMustHaveOneParent(diagnostic: Diagnostic) {
 	return diagnostic.code !== DiagnosticCodes.MUST_HAVE_PARENT_ELEMENT;
+}
+
+function isNoImportImplicitAnyType(diagnostic: Diagnostic) {
+	return diagnostic.code !== DiagnosticCodes.NO_DECL_IMPLICIT_ANY_TYPE
 }
 
 /**
