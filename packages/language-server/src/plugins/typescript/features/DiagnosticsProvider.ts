c7414a43b4ca6d2297467d1293e527356ec8f1f1
Princesseuh <princssdev@gmail.com>
2022-07-14 15:43:04 -0400

    Update to latest Astro version

--- packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
+++ packages/language-server/src/plugins/typescript/features/DiagnosticsProvider.ts
@@ -284,7 +284,7 @@ function enhanceIfNecessary(diagnostic: Diagnostic): Diagnostic {
 
 	// For the rare case where an user might try to put a client directive on something that is not a component
 	if (diagnostic.code === DiagnosticCodes.TYPE_NOT_ASSIGNABLE) {
-		if (diagnostic.message.includes("Property 'client:") && diagnostic.message.includes("to type 'HTMLProps")) {
+		if (diagnostic.message.includes("Property 'client:") && diagnostic.message.includes("to type 'HTMLAttributes")) {
 			return {
 				...diagnostic,
 				message: 'Client directives are only available on framework components',
