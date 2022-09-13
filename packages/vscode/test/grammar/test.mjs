1fad5f573728dcbca6aac3f67bc0b0ed26c8e2bc
Princesseuh <princssdev@gmail.com>
2022-09-13 13:48:18 -0300

    Attempt to migrate repo to pnpm

--- packages/vscode/test/grammar/test.mjs
+++ packages/vscode/test/grammar/test.mjs
@@ -43,7 +43,7 @@ async function snapShotTest() {
 		...extraArgs,
 	];
 
-	const code = await promisifySpawn(process.platform === 'win32' ? 'yarn.cmd' : 'yarn', args, {
+	const code = await promisifySpawn('pnpm', args, {
 		stdio: 'inherit',
 	});
 
