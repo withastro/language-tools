91189f8b1d5ff5412b6d3a2f7b9346c75fd65d38
Princesseuh <princssdev@gmail.com>
2023-05-12 12:41:55 +0200

    fix: commands

--- packages/vscode/src/client.ts
+++ packages/vscode/src/client.ts
@@ -1,27 +1,58 @@
 import { protocol } from '@astrojs/language-server';
 import { DiagnosticModel, InitializationOptions } from '@volar/language-server';
-import { activateAutoInsertion, supportLabsVersion, type ExportsInfoForLabs } from '@volar/vscode';
+import {
+	activateAutoInsertion,
+	activateFindFileReferences,
+	activateReloadProjects,
+	supportLabsVersion,
+	type ExportsInfoForLabs,
+} from '@volar/vscode';
+import * as path from 'node:path';
 import * as vscode from 'vscode';
 import * as lsp from 'vscode-languageclient/node';
 
 let client: lsp.BaseLanguageClient;
 
 export async function activate(context: vscode.ExtensionContext) {
-	const serverModule = vscode.Uri.joinPath(context.extensionUri, 'dist/node/server.js');
-	const runOptions = { execArgv: <string[]>[] };
+	const runtimeConfig = vscode.workspace.getConfiguration('astro.language-server');
+
+	const { workspaceFolders } = vscode.workspace;
+	const rootPath = workspaceFolders?.[0].uri.fsPath;
+
+	let lsPath = await getConfiguredServerPath(context.workspaceState);
+	if (typeof lsPath === 'string' && lsPath.trim() !== '' && typeof rootPath === 'string') {
+		lsPath = path.isAbsolute(lsPath) ? lsPath : path.join(rootPath, lsPath);
+		console.info(`Using language server at ${lsPath}`);
+	} else {
+		lsPath = undefined;
+	}
+	const serverModule = lsPath
+		? require.resolve(lsPath)
+		: vscode.Uri.joinPath(context.extensionUri, 'dist/node/server.js').fsPath;
+
+	const runOptions = { execArgv: [] };
 	const debugOptions = { execArgv: ['--nolazy', '--inspect=' + 6009] };
+
 	const serverOptions: lsp.ServerOptions = {
 		run: {
-			module: serverModule.fsPath,
+			module: serverModule,
 			transport: lsp.TransportKind.ipc,
 			options: runOptions,
 		},
 		debug: {
-			module: serverModule.fsPath,
+			module: serverModule,
 			transport: lsp.TransportKind.ipc,
 			options: debugOptions,
 		},
 	};
+
+	const serverRuntime = runtimeConfig.get<string>('runtime');
+	if (serverRuntime) {
+		serverOptions.run.runtime = serverRuntime;
+		serverOptions.debug.runtime = serverRuntime;
+		console.info(`Using ${serverRuntime} as runtime`);
+	}
+
 	const initializationOptions: InitializationOptions = {
 		typescript: {
 			tsdk: require('path').join(vscode.env.appRoot, 'extensions/node_modules/typescript/lib'),
@@ -42,6 +73,8 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	// support for auto close tag
 	activateAutoInsertion([client], (document) => document.languageId === 'astro');
+	activateFindFileReferences('astro.findFileReferences', client);
+	activateReloadProjects('astro.reloadProjects', [client]);
 
 	return {
 		volarLabs: {
@@ -54,4 +87,52 @@ export async function activate(context: vscode.ExtensionContext) {
 
 export function deactivate(): Thenable<any> | undefined {
 	return client?.stop();
+}
+
+async function getConfiguredServerPath(workspaceState: vscode.Memento) {
+	const scope = 'astro.language-server';
+	const detailedLSPath = vscode.workspace.getConfiguration(scope).inspect<string>('ls-path');
+
+	const lsPath =
+		detailedLSPath?.globalLanguageValue ||
+		detailedLSPath?.defaultLanguageValue ||
+		detailedLSPath?.globalValue ||
+		detailedLSPath?.defaultValue;
+
+	const workspaceLSPath =
+		detailedLSPath?.workspaceFolderLanguageValue ||
+		detailedLSPath?.workspaceLanguageValue ||
+		detailedLSPath?.workspaceFolderValue ||
+		detailedLSPath?.workspaceValue;
+
+	const useLocalLanguageServerKey = `${scope}.useLocalLS`;
+	let useWorkspaceServer = workspaceState.get<boolean>(useLocalLanguageServerKey);
+
+	if (useWorkspaceServer === undefined && workspaceLSPath !== undefined) {
+		const msg =
+			'This workspace contains an Astro Language Server version. Would you like to use the workplace version?';
+		const allowPrompt = 'Allow';
+		const dismissPrompt = 'Dismiss';
+		const neverPrompt = 'Never in This Workspace';
+
+		const result = await vscode.window.showInformationMessage(
+			msg,
+			allowPrompt,
+			dismissPrompt,
+			neverPrompt
+		);
+
+		if (result === allowPrompt) {
+			await workspaceState.update(useLocalLanguageServerKey, true);
+			useWorkspaceServer = true;
+		} else if (result === neverPrompt) {
+			await workspaceState.update(useLocalLanguageServerKey, false);
+		}
+	}
+
+	if (useWorkspaceServer === true) {
+		return workspaceLSPath || lsPath;
+	} else {
+		return lsPath;
+	}
 }
