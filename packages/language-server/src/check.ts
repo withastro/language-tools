3962b6fa7631464af21a0753727026b4ba6fde23
Princesseuh <princssdev@gmail.com>
2023-05-27 00:24:17 +0200

    feat: entrypoint for astro check

--- packages/language-server/src/check.ts
+++ packages/language-server/src/check.ts
@@ -1,5 +1,6 @@
 import * as kit from '@volar/kit';
-import * as path from 'path';
+import fg from 'fast-glob';
+import { pathToFileURL } from 'node:url';
 import { getLanguageModule } from './core/index.js';
 import { getSvelteLanguageModule } from './core/svelte.js';
 import { getAstroInstall } from './core/utils.js';
@@ -7,44 +8,104 @@ import { getVueLanguageModule } from './core/vue.js';
 import createAstroService from './plugins/astro.js';
 import createTypeScriptService from './plugins/typescript/index.js';
 
-const ts = require('typescript') as typeof import('typescript/lib/tsserverlibrary');
-const tsconfig = getTsconfig();
-const project = kit.createProject(tsconfig, [
-	{ extension: 'astro', isMixedContent: true, scriptKind: 7 },
-	{ extension: 'vue', isMixedContent: true, scriptKind: 7 },
-	{ extension: 'svelte', isMixedContent: true, scriptKind: 7 },
-]);
-const config: kit.Config = {
-	languages: {
-		astro: getLanguageModule(getAstroInstall([process.cwd()]), ts),
-		svelte: getSvelteLanguageModule(),
-		vue: getVueLanguageModule(),
-	},
-	services: {
-		typescript: createTypeScriptService(),
-		astro: createAstroService(),
-	},
-};
-const linter = kit.createLinter(config, project.languageServiceHost);
+// Export those for downstream consumers
+export { DiagnosticSeverity, type Diagnostic } from '@volar/language-server';
 
-async function lintProject(fileNames: string[], ignoredFiles: string[], logErrors = false) {
-	const files = fileNames.length > 0 ? fileNames : project.languageServiceHost.getScriptFileNames();
-
-	const errors: kit.Diagnostic[] = [];
-	for (const file of files) {
-		const fileErrors = await linter.check(file);
-		errors.push(...fileErrors);
-		if (logErrors) {
-			linter.logErrors(file, fileErrors);
-		}
-	}
-
-	return errors;
+export interface CheckResult {
+	errors: kit.Diagnostic[];
+	fileUrl: URL;
+	fileContent: string;
 }
 
-lintProject([], [], true);
+export class AstroCheck {
+	private ts!: typeof import('typescript/lib/tsserverlibrary.js');
+	private project!: ReturnType<typeof kit.createProject>;
+	private linter!: ReturnType<typeof kit.createLinter>;
 
-function getTsconfig() {
-	let tsconfig = path.resolve(process.cwd(), './tsconfig.json');
-	return tsconfig;
+	constructor(
+		private readonly workspacePath: string,
+		private readonly typescriptPath: string | undefined
+	) {
+		this.initialize();
+	}
+
+	/**
+	 * Lint a list of files or the entire project and optionally log the errors found
+	 * @param fileNames List of files to lint, if undefined, all files included in the project will be linted
+	 * @param logErrors Whether to log errors by itself. This is disabled by default.
+	 * @return {CheckResult} The result of the lint, including a list of errors, the file's content and its file path.
+	 */
+	public async lint(
+		fileNames: string[] | undefined = undefined,
+		logErrors = false
+	): Promise<CheckResult[]> {
+		const files =
+			fileNames !== undefined
+				? fileNames
+				: this.project.languageServiceHost
+						.getScriptFileNames()
+						.filter((file) => file.endsWith('.astro'));
+
+		const errors: CheckResult[] = [];
+		for (const file of files) {
+			const fileSnapshot = this.project.languageServiceHost.getScriptSnapshot(file);
+			const fileContent = fileSnapshot?.getText(0, fileSnapshot.getLength());
+
+			const fileErrors = await this.linter.check(file);
+			if (logErrors) {
+				this.linter.logErrors(file, fileErrors);
+			}
+
+			errors.push({
+				errors: fileErrors,
+				fileContent: fileContent ?? '',
+				fileUrl: pathToFileURL(file),
+			});
+		}
+
+		return errors;
+	}
+
+	private initialize() {
+		this.ts = this.typescriptPath ? require(this.typescriptPath) : require('typescript');
+		const tsconfig = this.getTsconfig();
+
+		const config: kit.Config = {
+			languages: {
+				astro: getLanguageModule(getAstroInstall([this.workspacePath]), this.ts),
+				svelte: getSvelteLanguageModule(),
+				vue: getVueLanguageModule(),
+			},
+			services: {
+				typescript: createTypeScriptService(),
+				astro: createAstroService(),
+			},
+		};
+
+		if (!tsconfig) {
+			this.project = kit.createInferredProject(this.workspacePath, () => {
+				return fg.sync('**/*.astro', {
+					cwd: this.workspacePath,
+					ignore: ['node_modules'],
+					absolute: true,
+				});
+			});
+		} else {
+			this.project = kit.createProject(tsconfig, [
+				{ extension: 'astro', isMixedContent: true, scriptKind: 7 },
+				{ extension: 'vue', isMixedContent: true, scriptKind: 7 },
+				{ extension: 'svelte', isMixedContent: true, scriptKind: 7 },
+			]);
+		}
+
+		this.linter = kit.createLinter(config, this.project.languageServiceHost);
+	}
+
+	private getTsconfig() {
+		const tsconfig =
+			this.ts.findConfigFile(this.workspacePath, this.ts.sys.fileExists) ||
+			this.ts.findConfigFile(this.workspacePath, this.ts.sys.fileExists, 'jsconfig.json');
+
+		return tsconfig;
+	}
 }
--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,1 +1,2 @@
 export * as protocol from '@volar/language-server/protocol';
+export { AstroCheck, CheckResult, Diagnostic, DiagnosticSeverity } from './check.js';
