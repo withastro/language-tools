bcc4a6022eb1e3823688850307c7aa0b0c086fd1
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-03-20 10:41:19 +0100

    test: add more tests and update Volar

--- packages/language-server/test/typescript/caching.test.ts
+++ packages/language-server/test/typescript/caching.test.ts
@@ -1,3 +1,4 @@
+import { readdir } from 'node:fs/promises';
 import path from 'node:path';
 import type { FullDocumentDiagnosticReport } from '@volar/language-server';
 import { expect } from 'chai';
@@ -6,15 +7,17 @@ import { type LanguageServer, getLanguageServer } from '../server.js';
 
 const fixtureDir = path.join(__dirname, '../fixture');
 
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
 describe('TypeScript - Cache invalidation', async () => {
 	let languageServer: LanguageServer;
 
-	function createFile(name: string, contents: string) {
-		return writeFile(path.join(fixtureDir, 'caching', name), contents);
+	async function createFile(name: string, contents: string) {
+		await writeFile(path.join(fixtureDir, 'caching', name), contents);
 	}
 
-	function removeFile(name: string) {
-		return rm(path.join(fixtureDir, 'caching', name));
+	async function removeFile(name: string) {
+		await rm(path.join(fixtureDir, 'caching', name));
 	}
 
 	before(async () => {
@@ -57,14 +60,20 @@ describe('TypeScript - Cache invalidation', async () => {
 
 		await removeFile('toBeDeleted.astro');
 
+		const directoryContent = await readdir(path.join(fixtureDir, '/caching'));
+		expect(directoryContent).to.not.include('toBeDeleted.astro');
+
 		const completions = await languageServer.handle.sendCompletionRequest(document.uri, {
 			line: 1,
 			character: 33,
 		});
 
 		const labels = completions?.items.map((i) => i.label);
-		expect(labels).to.not.include('toBeDeleted.astro', `Expected toBeDeleted.astro to not be in the completions`);
-	})
+		expect(labels).to.not.include(
+			'toBeDeleted.astro',
+			`Expected toBeDeleted.astro to not be in the completions, since the file was deleted`
+		);
+	});
 
 	it('Can get auto-imports for new files', async () => {
 		const fileNames = ['AutoImport.astro', 'AutoImport2.astro'];
@@ -84,22 +93,44 @@ describe('TypeScript - Cache invalidation', async () => {
 			});
 
 			const labels = imports?.items.map((i) => i.label);
-			const className = fileName.slice(0, -".astro".length)
+			const className = fileName.slice(0, -'.astro'.length);
 			expect(labels).to.include(className, `Expected ${className} to be in the auto-imports`);
 		}
-	})
+	});
 
 	it('New files have access to context of the project', async () => {
-		await createFile('WillImportFromSuperModule.astro', '---\n\nimport { hello } from "im-a-super-module";\n\nhello;\n\n---\n');
+		const existingDocument = await languageServer.handle.openTextDocument(
+			path.join(fixtureDir, 'importFromSuperModule.astro'),
+			'astro'
+		);
+
+		const existingDiagnostics = (await languageServer.handle.sendDocumentDiagnosticRequest(
+			existingDocument.uri
+		)) as FullDocumentDiagnosticReport;
+
+		expect(existingDiagnostics.items).to.have.length(
+			0,
+			'Expected no diagnostics, as the file is part of the project'
+		);
+
+		await createFile(
+			'WillImportFromSuperModule.astro',
+			'---\n\nimport { hello } from "im-a-super-module";\n\nhello;\n\n---\n'
+		);
 
 		const document = await languageServer.handle.openTextDocument(
 			path.join(fixtureDir, 'caching', 'WillImportFromSuperModule.astro'),
 			'astro'
 		);
 
-		const diagnostics = await languageServer.handle.sendDocumentDiagnosticRequest(document.uri) as FullDocumentDiagnosticReport;
+		const diagnostics = (await languageServer.handle.sendDocumentDiagnosticRequest(
+			document.uri
+		)) as FullDocumentDiagnosticReport;
 
-		expect(diagnostics.items).to.have.length(0, 'Expected no diagnostics, as new files should have access to the module declaration in the project.');
+		expect(diagnostics.items).to.have.length(
+			0,
+			'Expected no diagnostics, as new files should have access to the module declaration in the project like already existing files.'
+		);
 	});
 
 	after(async () => {
