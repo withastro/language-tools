fa7175b4484d06c635f21f706dae270014c83c0c
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-08-07 19:00:21 +0200

    feat: references support

---
+++ packages/ts-plugin/src/frontmatter.ts
@@ -0,0 +1,139 @@
+import {
+	forEachEmbeddedCode,
+	type CodeMapping,
+	type LanguagePlugin,
+	type VirtualCode,
+} from '@volar/language-core';
+import type ts from 'typescript';
+import { yaml2ts } from '@astrojs/yaml2ts';
+
+export type CollectionConfig = {
+	folder: string;
+	config: {
+		collections: {
+			hasSchema: boolean;
+			name: string;
+		}[];
+		entries: Record<string, string>;
+	};
+};
+
+function getCollectionName(collectionConfigs: CollectionConfig[], fsPath: string) {
+	// console.log('===== ASTRO TS PLUGIN =====', fsPath);
+	for (const collection of collectionConfigs) {
+		if (collection.config.entries[fsPath]) {
+			return collection.config.entries[fsPath];
+		}
+	}
+}
+
+export function getFrontmatterLanguagePlugin(
+	collectionConfigs: CollectionConfig[]
+): LanguagePlugin<string, FrontmatterHolder> {
+	return {
+		getLanguageId(scriptId) {
+			if (scriptId.endsWith('.md') || scriptId.endsWith('.mdx') || scriptId.endsWith('.mdoc')) {
+				return 'frontmatter';
+			}
+		},
+		createVirtualCode(scriptId, languageId, snapshot) {
+			if (languageId === 'frontmatter') {
+				const fileName = scriptId.replace(/\\/g, '/');
+				return new FrontmatterHolder(
+					fileName,
+					snapshot,
+					getCollectionName(collectionConfigs, fileName)
+				);
+			}
+		},
+		updateVirtualCode(scriptId, virtualCode, newSnapshot, ctx) {
+			return virtualCode.updateSnapshot(newSnapshot);
+		},
+		typescript: {
+			extraFileExtensions: [
+				{ extension: 'md', isMixedContent: true, scriptKind: 7 },
+				{ extension: 'mdx', isMixedContent: true, scriptKind: 7 },
+				{ extension: 'mdoc', isMixedContent: true, scriptKind: 7 },
+			],
+			getServiceScript(astroCode) {
+				for (const code of forEachEmbeddedCode(astroCode)) {
+					if (code.id === 'frontmatter-ts') {
+						return {
+							code,
+							extension: '.ts',
+							scriptKind: 3 satisfies ts.ScriptKind.TS,
+						};
+					}
+				}
+				return undefined;
+			},
+		},
+	};
+}
+
+export class FrontmatterHolder implements VirtualCode {
+	id: string = 'frontmatter-holder';
+	languageId: string = 'frontmatter';
+	mappings!: CodeMapping[];
+	embeddedCodes!: VirtualCode[];
+	public hasFrontmatter: boolean = false;
+
+	lastValidContent: {
+		source: string;
+		generated: string;
+		mappings: CodeMapping[];
+	} = {
+		source: '',
+		generated: '',
+		mappings: [],
+	};
+
+	constructor(
+		public fileName: string,
+		public snapshot: ts.IScriptSnapshot,
+		public collection: string | undefined
+	) {
+		this.updateSnapshot(snapshot);
+	}
+
+	updateSnapshot(snapshot: ts.IScriptSnapshot) {
+		this.mappings = [
+			{
+				sourceOffsets: [0],
+				generatedOffsets: [0],
+				lengths: [this.snapshot.getLength()],
+				data: {
+					verification: true,
+					completion: true,
+					semantic: true,
+					navigation: true,
+					structure: true,
+					format: true,
+				},
+			},
+		];
+
+		this.embeddedCodes = [];
+		this.snapshot = snapshot;
+
+		if (!this.collection) {
+			return this;
+		}
+
+		// TODO: More robust frontmatter detection
+		this.hasFrontmatter = this.snapshot.getText(0, 10).startsWith('---');
+
+		const frontmatter = this.hasFrontmatter
+			? this.snapshot
+					.getText(0, this.snapshot.getText(0, this.snapshot.getLength()).indexOf('---', 3) + 3)
+					.replaceAll('---', '   ')
+			: ''; // Generate an empty frontmatter so that we can map an error for a missing frontmatter
+
+		if (this.hasFrontmatter) {
+			const yaml2tsResult = yaml2ts(frontmatter, this.snapshot, this.collection);
+			this.embeddedCodes.push(yaml2tsResult.virtualCode);
+		}
+
+		return this;
+	}
+}
--- packages/ts-plugin/src/index.ts
+++ packages/ts-plugin/src/index.ts
@@ -1,6 +1,18 @@
 import { createLanguageServicePlugin } from '@volar/typescript/lib/quickstart/createLanguageServicePlugin.js';
 import { getLanguagePlugin } from './language.js';
+import { CollectionConfig, getFrontmatterLanguagePlugin } from './frontmatter.js';
 
-export = createLanguageServicePlugin(() => ({
-	languagePlugins: [getLanguagePlugin()],
-}));
+export = createLanguageServicePlugin((ts, info) => {
+	const collectionConfigs = {
+		folder: info.project.getCurrentDirectory(),
+		config: JSON.parse(
+			ts.sys.readFile(
+				info.project.getCurrentDirectory() + '/.astro/collections/collections.json'
+			) as string
+		) as CollectionConfig['config'],
+	};
+
+	return {
+		languagePlugins: [getLanguagePlugin(), getFrontmatterLanguagePlugin([collectionConfigs])],
+	};
+});
