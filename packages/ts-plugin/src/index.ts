fc2b6a9302dc0d1bee4427e745d742d8632c18d4
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-08-08 17:24:57 +0200

    refactor: ts-plugin

--- packages/ts-plugin/src/index.ts
+++ packages/ts-plugin/src/index.ts
@@ -8,14 +8,14 @@ export = createLanguageServicePlugin((ts, info) => {
 	let collectionConfig = undefined;
 
 	try {
-		collectionConfig = {
-			folder: info.project.getCurrentDirectory(),
-			config: JSON.parse(
-				ts.sys.readFile(
-					info.project.getCurrentDirectory() + '/.astro/collections/collections.json',
-				) as string,
-			) as CollectionConfig['config'],
-		};
+		const currentDir = info.project.getCurrentDirectory();
+		const fileContent = ts.sys.readFile(currentDir + '/.astro/collections/collections.json');
+		if (fileContent) {
+			collectionConfig = {
+				folder: currentDir,
+				config: JSON.parse(fileContent) as CollectionConfig['config'],
+			};
+		}
 	} catch (err) {
 		// If the file doesn't exist, we don't really care, but if it's something else, we want to know
 		if (err && (err as any).code !== 'ENOENT') console.error(err);
