92867ad08f7e43b045ebafdcb86fac3ef4e6ec84
Chris Swithinbank <swithinbank@gmail.com>
2025-09-24 17:02:25 +0200

    Add logic for auto-updating schema config from collection metadata

--- packages/vscode/src/client.ts
+++ packages/vscode/src/client.ts
@@ -1,4 +1,5 @@
 import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import * as protocol from '@volar/language-server/protocol';
 import type { LabsInfo } from '@volar/vscode';
 import {
@@ -14,6 +15,7 @@ import * as vscode from 'vscode';
 import * as lsp from 'vscode-languageclient/node';
 
 let client: lsp.BaseLanguageClient;
+let collectionsWatcher: vscode.FileSystemWatcher;
 
 type InitOptions = {
 	typescript: {
@@ -73,6 +75,10 @@ export async function activate(context: vscode.ExtensionContext): Promise<LabsIn
 		contentIntellisense: hasContentIntellisense,
 	} satisfies InitOptions;
 
+	if (hasContentIntellisense && rootPath) {
+		await initializeCollectionSchemaAutoconfig(rootPath);
+	}
+
 	const clientOptions = {
 		documentSelector: [
 			{ language: 'astro' },
@@ -98,8 +104,8 @@ export async function activate(context: vscode.ExtensionContext): Promise<LabsIn
 	return volarLabs.extensionExports;
 }
 
-export function deactivate(): Thenable<any> | undefined {
-	return client?.stop();
+export async function deactivate() {
+	return Promise.allSettled([client?.stop(), collectionsWatcher?.dispose()]);
 }
 
 async function getConfiguredServerPath(workspaceState: vscode.Memento) {
@@ -148,4 +154,101 @@ async function getConfiguredServerPath(workspaceState: vscode.Memento) {
 	} else {
 		return lsPath;
 	}
+}
+
+/**
+ * Factory that creates an updater function to run when collection metadata changes.
+ * @param rootPath The root path of the user’s Astro project.
+ */
+const createSchemaConfigUpdater = (rootPath: string) => async (uri: vscode.Uri) => {
+	try {
+		// Read the Astro-generated collections metadata.
+		const collectionsMetaBytes = await vscode.workspace.fs.readFile(uri);
+		const collectionsMetaString = new TextDecoder().decode(collectionsMetaBytes);
+		const collectionsMeta = JSON.parse(collectionsMetaString) as {
+			collections: Array<{ hasSchema: boolean; name: string }>;
+			entries: Record<string, string>;
+		};
+
+		const configScope = 'json.schemas';
+		/** The current `json.schemas` configuration in this workspace. */
+		const jsonConfig =
+			vscode.workspace.getConfiguration().inspect<any[]>(configScope)?.workspaceValue || [];
+		/** User-authored entries from the `json.schemas` array. */
+		const userEntriesJSON = jsonConfig.filter((item) => !('__astro__' in item));
+
+		/** Entries for `json.schemas` based on collection metadata. */
+		const autoGenEntriesJSON = collectionsMeta.collections
+			.map(({ name }) => ({
+				fileMatch: Object.entries(collectionsMeta.entries)
+					.filter(([file, collection]) => collection === name && file.endsWith('.json'))
+					.map(([file]) => fileURLToPath(file).replace(rootPath.toLowerCase(), '')),
+				url: `./.astro/collections/${name}.schema.json`,
+				__astro__: true,
+			}))
+			// Skip entries with no file matches.
+			.filter(({ fileMatch }) => fileMatch.length > 0);
+
+		// Update `json.schemas` in workspace settings, adding auto-generated values.
+		await vscode.workspace
+			.getConfiguration()
+			.update(configScope, [...userEntriesJSON, ...autoGenEntriesJSON]);
+
+		const yamlSchemasConfigScope = 'yaml.schemas';
+		/** The current `yaml.schemas` configuration in this workspace. */
+		const yamlConfig =
+			vscode.workspace
+				.getConfiguration()
+				.inspect<Record<string, string | string[]>>(yamlSchemasConfigScope)?.workspaceValue || {};
+		/** User-authored entries from the `yaml.schemas` object. */
+		const userEntriesYAML = Object.entries(yamlConfig).filter(
+			([, val]) => typeof val === 'string' || val.at(-1) !== '__astro__',
+		);
+
+		/** Entries for `yaml.schemas` based on collection metadata. */
+		const autoGenEntriesYAML = collectionsMeta.collections
+			.map(
+				({ name }) =>
+					[
+						`./.astro/collections/${name}.schema.json`,
+						Object.entries(collectionsMeta.entries)
+							.filter(
+								([file, collection]) =>
+									collection === name && (file.endsWith('.yml') || file.endsWith('.yaml')),
+							)
+							.map(([file]) => fileURLToPath(file).replace(rootPath.toLowerCase(), '')),
+					] as const,
+			)
+			.filter(([, files]) => files.length > 0)
+			.map(([schema, files]) => [schema, [...files, '__astro__']] as const);
+
+		// Update `yaml.schemas` in workspace settings, adding auto-generated values.
+		await vscode.workspace
+			.getConfiguration()
+			.update(
+				yamlSchemasConfigScope,
+				Object.fromEntries([...userEntriesYAML, ...autoGenEntriesYAML]),
+			);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+/**
+ * Watch for changes to the `.astro/collections/collections.json` metadata and mirror it to the
+ * VS Code workspace configuration for JSON and YAML schemas.
+ * @param rootPath The root path of the user’s Astro project.
+ */
+async function initializeCollectionSchemaAutoconfig(rootPath: string) {
+	const updateSchemaConfig = createSchemaConfigUpdater(rootPath);
+
+	const collectionsPath = '.astro/collections/collections.json';
+	collectionsWatcher = vscode.workspace.createFileSystemWatcher(
+		new vscode.RelativePattern(rootPath, collectionsPath),
+	);
+	collectionsWatcher.onDidChange(updateSchemaConfig);
+	collectionsWatcher.onDidCreate(updateSchemaConfig);
+	collectionsWatcher.onDidDelete(updateSchemaConfig);
+
+	await updateSchemaConfig(vscode.Uri.file(`${rootPath}/${collectionsPath}`));
 }
