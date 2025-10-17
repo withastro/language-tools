373f51b47330485b4bf8a7f80c034406c8e8c728
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2025-10-17 17:15:49 +0200

    fix: try killing the porocess

--- packages/language-server/test/misc/teardown.ts
+++ packages/language-server/test/misc/teardown.ts
@@ -1,11 +1,13 @@
 import { describe, it } from 'node:test';
-import teardown from '../takedown.js';
+import { getLanguageServer } from '../server.js';
 
 describe('Teardown', () => {
 	// This file is just to make sure the teardown file is included in the test run until we can upgrade to a newer Node.js version.
 	// The actual teardown logic is in `takedown.ts`.
 
 	it('Can teardown', async () => {
-		await teardown();
+		const languageServer = await getLanguageServer();
+		languageServer.handle.connection.dispose();
+		languageServer.handle.process.kill();
 	});
 });
--- packages/language-server/test/takedown.ts
+++
@@ -1,6 +0,0 @@
-import { getLanguageServer } from './server.js';
-
-export default async function teardown() {
-	const languageServer = await getLanguageServer();
-	languageServer.handle.connection.dispose();
-}
